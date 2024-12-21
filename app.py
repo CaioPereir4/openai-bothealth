d111bbd06c1a6e160e5e4ee4bf1550ebee5d9c38
CaioPereir4 <caiocesararagao03@gmail.com>
2024-12-21 16:54:15 -0300

    feat: add a route to generate a thread_id to have the messages historys.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #imports
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from assistent_bot_health import create_thread
 
 load_dotenv()
 
@@ -9,6 +10,12 @@ app = Flask(__name__)
 @app.route("/opeani-bothealth", methods=["GET"])
 def check_route():
     return "the route is up and running", 200
+
+@app.route("/opeani-bothealth/thread_id", methods=["GET"])
+def generate_thread_id():
+    thread_id = create_thread()
+    response = { "thread_id" : thread_id.id }
+    return  jsonify(response), 200
 
 if __name__ == "__main__":
     app.run(debug = True)
---
+++ assistent_bot_health.py
@@ -0,0 +1,13 @@
+#imports 
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+
+def create_thread():
+    return client.beta.threads.create()
